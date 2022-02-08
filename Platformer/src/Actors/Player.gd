76fab3095c3a0806507461b4b98c673f46ddad82
rohitchaoji <53764185+rohitchaoji@users.noreply.github.com>
2022-02-08 17:08:08 +0530

    Add files via upload

---
+++ Platformer/src/Actors/Player.gd
@@ -0,0 +1,44 @@
+extends Actor
+
+export var stomp_impulse = 1000.0
+
+func _on_EnemyDetector_area_entered(area):
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+	
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+
+
+
+func _physics_process(delta):
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
+	var direction: = get_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-Input.get_action_strength("jump") if is_on_floor() and Input.is_action_just_pressed("jump") else 0.0
+	)
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+	var out: = linear_velocity
+	out.x = speed.x * direction.x
+	if direction.y != 0.0:
+		out.y = speed.y * direction.y
+	if is_jump_interrupted:
+		out.y = 0.0
+	return out
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var out: = linear_velocity
+	out.y = -impulse
+	return out
+
+
